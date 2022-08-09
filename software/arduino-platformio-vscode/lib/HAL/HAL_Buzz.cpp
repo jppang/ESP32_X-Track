5cde83e9512a67857f85298b6224c70f7fafc7e0
jppang <jppang@me.com>
2022-08-09 19:01:55 +0800

    Fixed buzzer tone issue.
    Using ESP32 arduino core's tone function.

--- software/arduino-platformio-vscode/lib/HAL/HAL_Buzz.cpp
+++ software/arduino-platformio-vscode/lib/HAL/HAL_Buzz.cpp
@@ -5,9 +5,8 @@ static bool IsEnable = true;
 
 void HAL::Buzz_init()
 {
+    setToneChannel(BUZZ_CHANNEL);
     pinMode(CONFIG_BUZZ_PIN, OUTPUT);
-    ledcSetup(BUZZ_CHANNEL, 5000, 10);
-    ledcAttachPin(CONFIG_BUZZ_PIN, BUZZ_CHANNEL);
 }
 
 void HAL::Buzz_SetEnable(bool en)
@@ -26,25 +25,12 @@ void HAL::Buzz_Tone(uint32_t freq, int32_t duration)
     {
         return;
     }
-    uint32_t buzzNow = millis();
     if(duration >= 0)
     {
-        //tone(CONFIG_BUZZ_PIN, freq, duration);
-        ledcWriteTone(BUZZ_CHANNEL, freq);
-        //vTaskDelay(duration / portTICK_PERIOD_MS);
-        do {
-            ;
-        } while((millis() - buzzNow) < duration);
-        ledcWriteTone(BUZZ_CHANNEL, 0);
+        tone(CONFIG_BUZZ_PIN, freq, duration);
     }
     else
     {
-        //tone(CONFIG_BUZZ_PIN, freq);
-        ledcWriteTone(BUZZ_CHANNEL, freq);
-        //vTaskDelay(10 / portTICK_PERIOD_MS);
-        do {
-            ;
-        } while((millis() - buzzNow) < 10);
-        ledcWriteTone(BUZZ_CHANNEL, 0);
+        tone(CONFIG_BUZZ_PIN, freq, 10);
     }
 }
